b5b80c18ca3fd43f3139f2f5e4b15ff415edf472
Dhivyadinesh <53462975+Dhivyadinesh@users.noreply.github.com>
2020-04-18 21:52:31 +0530

    Add files via upload

--- jsoup-jdbc/src/main/java/SaasDataExtractFromXmlFile/Followers.java
+++ jsoup-jdbc/src/main/java/SaasDataExtractFromXmlFile/Followers.java
@@ -14,7 +14,9 @@ import org.jsoup.select.Elements;
 public class Followers {
 
 	public static void main(String[] args) throws IOException, SQLException {
-
+		String url = "jdbc:mysql://localhost:3306/saas";
+		String username = "root";
+		String password = "dineshdd";
 		File file = new File("/home/dinesh/Documents/50product.txt");
 		Scanner scan;
 		try {
@@ -27,19 +29,62 @@ public class Followers {
 				String productName;
 				productName = doc.getElementsByClass("h-title").text();
 				System.out.println(productName);
-				boolean linkedin = false, twitter, facebook, instagram, youtube;
+				boolean linkedin = false,twitter, facebook, instagram, youtube;
 				String linkedinCount, twitterCount, facebookCount, instagramCount, youtubeCount;
-				Elements linkedinUrl, twitterUrl, facebookUrl, instagramUrl, youtubeUrl;
-
+				String linkedinUrl, twitterUrl, facebookUrl, instagramUrl, youtubeUrl;
 				Elements followers = doc.getElementsByClass("right_followers").select("a");
-				for(Element element : followers) {
-					boolean String = false;
-					String url = element.attr("abs:href");
-					System.out.println(url);
-					String count = element.getElementsByTag("span").text();
-					System.out.println(count);
+					for(Element element : followers) {
+						linkedinUrl = element.attr("abs:href");
+						if(linkedinUrl.contains("linkedin")){
+							linkedin = element.hasText();
+					    System.out.println(linkedin);
+						System.out.println(linkedinUrl);
+						linkedinCount = element.getElementsByTag("span").text();
+						System.out.println(linkedinCount);
+						}
 					}
+					for(Element element : followers) {
+						twitterUrl = element.attr("abs:href");
+						if(twitterUrl.contains("twitter")){
+							twitter = element.hasText();
+						    System.out.println(twitter);
+						System.out.println(twitterUrl);
+						twitterCount = element.getElementsByTag("span").text();
+						System.out.println(twitterCount);
+						}
+					}
+					for(Element element : followers) {
+						facebookUrl = element.attr("abs:href");
+						if(facebookUrl.contains("facebook")){
+							facebook = element.hasText();
+						    System.out.println(facebook);
+						System.out.println(facebookUrl);
+						facebookCount = element.getElementsByTag("span").text();
+						System.out.println(facebookCount);
+						}
+					}
+					for(Element element : followers) {
+						instagramUrl = element.attr("abs:href");
+						if(instagramUrl.contains("instagram")){
+							instagram = element.hasText();
+						    System.out.println(instagram);
+						System.out.println(instagramUrl);
+						instagramCount = element.getElementsByTag("span").text();
+						System.out.println(instagramCount);
+						}
+					}
+				
+					for(Element element : followers) {
+						youtubeUrl = element.attr("abs:href");
+						if(youtubeUrl.contains("youtube")){
+							youtube = element.hasText();
+						    System.out.println(youtube);
+						System.out.println(youtubeUrl);
+						youtubeCount = element.getElementsByTag("span").text();
+						System.out.println(youtubeCount);
+						}
 				}
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
