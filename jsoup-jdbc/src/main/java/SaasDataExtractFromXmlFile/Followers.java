36d3cc50daf05bd19a5f9d94ab1731aa2659c101
Dhivyadinesh <53462975+Dhivyadinesh@users.noreply.github.com>
2020-04-22 23:39:31 +0530

    Add files via upload

--- jsoup-jdbc/src/main/java/SaasDataExtractFromXmlFile/Followers.java
+++ jsoup-jdbc/src/main/java/SaasDataExtractFromXmlFile/Followers.java
@@ -3,9 +3,11 @@ package SaasDataExtractFromXmlFile;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -21,74 +23,52 @@ public class Followers {
 		Scanner scan;
 		try {
 			scan = new Scanner(file);
-
 			while (scan.hasNextLine()) {
-				String productUrl = scan.nextLine();
+
+				String linkedinCount = "0", twitterCount = "0", facebookCount = "0", instagramCount = "0",
+						youtubeCount = "0";
+				String linkedinUrl = "null", twitterUrl = "null", facebookUrl = "null", instagramUrl = "null",
+						youtubeUrl = "null";
+				String productName = "0", productUrl = "0";
+
+				productUrl = scan.nextLine();
 				Document doc = Jsoup.connect(productUrl).get();
 				System.out.println(productUrl);
-				String productName;
 				productName = doc.getElementsByClass("h-title").text();
 				System.out.println(productName);
-				boolean linkedin = false,twitter, facebook, instagram, youtube;
-				String linkedinCount, twitterCount, facebookCount, instagramCount, youtubeCount;
-				String linkedinUrl, twitterUrl, facebookUrl, instagramUrl, youtubeUrl;
-				
 				Elements followers = doc.getElementsByClass("right_followers").select("a");
-				
-					for(Element element : followers) {
-						linkedinUrl = element.attr("abs:href");
-						if(linkedinUrl.contains("linkedin")){
-							linkedin = element.hasAttr("href");
-					    System.out.println(linkedin);
-						System.out.println(linkedinUrl);
-						}else {
-							System.out.println(0);
-						}	
-						
-						twitterUrl = element.attr("abs:href");
-						if(twitterUrl.contains("twitter")){
-							twitter = element.hasAttr("href");
-						    System.out.println(twitter);
-						System.out.println(twitterUrl);
-						twitterCount = element.getElementsByTag("span").text();
-						System.out.println(twitterCount);
-						}else {
-							System.out.println(0);
-						}
-						
-						facebookUrl = element.attr("abs:href");
-						if(facebookUrl.contains("facebook")){
-							facebook = element.hasAttr("href");
-						    System.out.println(facebook);
-						System.out.println(facebookUrl);
-						facebookCount = element.getElementsByTag("span").text();
-						System.out.println(facebookCount);
-						}else {
-							System.out.println(0);
-						}
-						
-						instagramUrl = element.attr("abs:href");
-						if(instagramUrl.contains("instagram")){
-							instagram = element.hasAttr("href");
-						    System.out.println(instagram);
-						System.out.println(instagramUrl);
-						instagramCount = element.getElementsByTag("span").text();
-						System.out.println(instagramCount);
-						}else {
-							System.out.println(0);
-						}
-				
-						youtubeUrl = element.attr("abs:href");
-						if(youtubeUrl.contains("youtube")){
-							youtube = element.hasAttr("href");
-						    System.out.println(youtube);
-						System.out.println(youtubeUrl);
-						youtubeCount = element.getElementsByTag("span").text();
-						System.out.println(youtubeCount);
-						}else {
-							System.out.println(0);
-						}
-				}
+String linkedin;
+				for (Element element : followers) {
+				 public void Follow(String Url ,String linkedin) {
+					 if(Url.contains("linkedin")) {
+						 
+					 }
+					 return;
+				 }
+					
+				} 
+				Connection conn = DriverManager.getConnection(url, username, password);
+				String sql;
+				sql = "INSERT INTO followers (productUrl,productName,linkedin,twitter,facebook,instagram,youtube,linkedinCount,twitterCount,facebookCount, instagramCount,youtubeCount,linkedinUrl,twitterUrl,facebookUrl,instagramUrl,youtubeUrl ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ";
+				PreparedStatement statement = conn.prepareStatement(sql);
+				statement.setString(1, productUrl);
+				statement.setString(2, productName);
+				statement.setBoolean(3, linkedin);
+				statement.setBoolean(4, twitter);
+				statement.setBoolean(5, facebook);
+				statement.setBoolean(6, instagram);
+				statement.setBoolean(7, youtube);
+				statement.setString(8, linkedinCount);
+				statement.setString(9, twitterCount);
+				statement.setString(10, facebookCount);
+				statement.setString(11, instagramCount);
+				statement.setString(12, youtubeCount);
+				statement.setString(13, linkedinUrl);
+				statement.setString(14, twitterUrl);
+				statement.setString(15, facebookUrl);
+				statement.setString(16, instagramUrl);
+				statement.setString(17, youtubeUrl);
+				statement.executeUpdate();
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
