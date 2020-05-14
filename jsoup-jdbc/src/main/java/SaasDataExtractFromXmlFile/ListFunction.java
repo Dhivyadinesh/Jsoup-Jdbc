e23b11ac9b4eefac037a8138e531b62fc462e5b4
Dhivyadinesh <53462975+Dhivyadinesh@users.noreply.github.com>
2020-05-14 23:40:19 +0530

    Add files via upload

--- jsoup-jdbc/src/main/java/SaasDataExtractFromXmlFile/ListFunction.java
+++ jsoup-jdbc/src/main/java/SaasDataExtractFromXmlFile/ListFunction.java
@@ -1,112 +1,103 @@
 package SaasDataExtractFromXmlFile;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
+	
 
-public class ListFunction {
+	public class ListFunction {
 		
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		
 		boolean linkedin = false, twitter = false, facebook = false, instagram = false, youtube = false;
-		String productName = "0", productUrl = "0";
-
-		String linkedinCount = "0", twitterCount = "0", facebookCount = "0", instagramCount = "0",
+		
+     	String linkedinCount = "0", twitterCount = "0", facebookCount = "0", instagramCount = "0",
 				youtubeCount = "0";
 		String linkedinUrl = "null", twitterUrl = "null", facebookUrl = "null", instagramUrl = "null",
 				youtubeUrl = "null";
+	
+		String productName = "0", productUrl = "0";
+		File file = new File("/home/dinesh/Documents/50product.txt");
+		Scanner scan;
 		try {
-			File file = new File("/home/dinesh/Documents/50product.txt");
-			Scanner scan;
 			scan = new Scanner(file);
-			ArrayList<String> urlList = new ArrayList<String>();
-			ArrayList<String> urlCount = new ArrayList<String>();
-		    while (scan.hasNextLine()) {
+			while (scan.hasNextLine()) {
 				productUrl = scan.nextLine();
-		    	Document doc = Jsoup.connect(productUrl).get();
+				Document doc = Jsoup.connect(productUrl).get();
 				System.out.println(productUrl);
 				productName = doc.getElementsByClass("h-title").text();
 				System.out.println(productName);
-				String Url;
+				String url;
 				Elements followers = doc.getElementsByClass("right_followers").select("a");
-
 				for (Element element : followers) {
-					Url = element.attr("abs:href");
-					if (Url.contains("linkedin")) {
-						linkedin = Url.contains("linkedin");
-						linkedinUrl = element.attr("href");
-						linkedinCount = element.getElementsByTag("span").text();
-						urlList.add(linkedinUrl);
-						urlCount.add(linkedinCount);
-						System.out.println("Product has linkedin " + linkedin);
-						System.out.println("linkedin url is" + linkedinUrl);
-						System.out.println("linkedinCount" + linkedinCount);
-					}
-
-					if (Url.contains("twitter")) {
-						twitter = Url.contains("twitter");
-						twitterUrl = element.attr("href");
-						twitterCount = element.getElementsByTag("span").text();
-						urlList.add(twitterUrl);
-						urlCount.add(twitterCount);
-						System.out.println(twitter);
-						System.out.println(twitterUrl);
-						System.out.println(twitterCount);
-					}
-
-					if (Url.contains("facebook")) {
-						facebook = Url.contains("facebook");
-						facebookUrl = element.attr("href");
-						facebookCount = element.getElementsByTag("span").text();
-						urlList.add(facebookUrl);
-						urlCount.add(facebookCount);
-						System.out.println(facebook);
-						System.out.println(urlList.add(facebookUrl));
-						System.out.println(urlCount.add(facebookCount));
-					}
-
-					if (Url.contains("instagram")) {
-						instagram = Url.contains("instagram");
-						instagramUrl = element.attr("href");
-						instagramCount = element.getElementsByTag("span").text();
-						urlList.add(instagramUrl);
-						urlCount.add(instagramCount);
-						System.out.println(instagram);
-						System.out.println(instagramUrl);
-						System.out.println(instagramCount);
-
-					}
-
-					if (Url.contains("youtube")) {
-						youtube = Url.contains("youtube");
-						youtubeUrl = element.attr("href");
-						youtubeCount = element.getElementsByTag("span").text();
-						System.out.println(youtube);
-						urlList.add(youtubeUrl);
-						urlCount.add(youtubeCount);
-						System.out.println(youtubeUrl);
-						System.out.println(youtubeCount);
-
-					}
-				}
-
-		    }
-		}catch(Exception e) {
-			System.out.println(0);
-		}
-		
+					url = element.attr("abs:href");
+      				linkedinMethod(url,linkedin,element,linkedinUrl,linkedinCount);
+      				twitterMethod(url,twitter,element,twitterUrl,twitterCount);
+      				facebookMethod(url,facebook,element,facebookUrl,facebookCount);
+      				instagramMethod(url,instagram,element,instagramUrl,instagramCount);
+      				youtubeMethod(url,youtube,element,youtubeUrl,youtubeCount);
+                	}
+				}	
+	   }catch(Exception e) {
+		   System.out.println("No url Found");
+	   }
 	}
-	public static void ListContent(ArrayList<String> s,ArrayList<String> count) {
-		int i;
-		for(i=0;i<s.size();i++) {
-			System.out.println("url"+s.get(i)+",count"+count.get(i));
+
+	private static void linkedinMethod(String url, boolean linkedin,Element element,String linkedinUrl,String linkedinCount) {
+		if (url.contains("linkedin")) {
+			linkedin = url.contains("linkedin");
+			linkedinUrl = element.attr("href");
+			linkedinCount = element.getElementsByTag("span").text();
+			System.out.println("Product has linkedin " + linkedin);
+			System.out.println("linkedin url is" + linkedinUrl);
+			System.out.println("linkedinCount" + linkedinCount);
+	    }
+	}
+	private static void twitterMethod(String url, boolean twitter,Element element,String twitterUrl,String twitterCount) {
+			if (url.contains("twitter")) {
+				twitter = url.contains("twitter");
+				twitterUrl = element.attr("href");
+				twitterCount = element.getElementsByTag("span").text();
+				System.out.println(twitter);
+				System.out.println(twitterUrl);
+				System.out.println(twitterCount);
+		    }
 		}
-		
-	} 
+		private static void facebookMethod(String url, boolean facebook,Element element,String facebookUrl,String facebookCount) {
+			if (url.contains("facebook")) {
+				facebook = url.contains("facebook");
+				facebookUrl = element.attr("href");
+				facebookCount = element.getElementsByTag("span").text();
+				System.out.println(facebook);
+				System.out.println(facebookUrl);
+				System.out.println(facebookCount);
+		    }
+		}
+		private static void instagramMethod(String url, boolean instagram,Element element,String instagramUrl,String instagramCount) {
+			if (url.contains("instagram")) {
+				instagram = url.contains("instagram");
+				instagramUrl =element.attr("href");
+				instagramCount = element.getElementsByTag("span").text();
+				System.out.println(instagram);
+				System.out.println(instagramUrl);
+				System.out.println(instagramCount);
+		    }
+		}
+		private static void youtubeMethod(String url, boolean youtube,Element element,String youtubeUrl,String youtubeCount) {
+			if (url.contains("youtube")) {
+				youtube = url.contains("youtube");
+				youtubeUrl = element.attr("href");
+				youtubeCount = element.getElementsByTag("span").text();
+				System.out.println(youtube);
+				System.out.println(youtubeUrl);
+				System.out.println(youtubeCount);
+		    }
+		}
+	
 }
